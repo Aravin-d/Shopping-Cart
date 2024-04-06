85ea7f0fe10f554dd08df488635974ffadc73bb7
Aravind <aravindprakash000@gmail.com>
2024-04-06 22:32:23 +0530

    Added Admin route and Admin panel view

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-const products = []
+const products = [
+  {
+    name : "Iphone",
+    category : "mobile",
+    description : "Apple product",
+    image : "https://picsum.photos/200/200"
+  },
+  {
+    name : "Samsung",
+    category : "mobile",
+    description : "Samsung product",
+    image : "https://picsum.photos/200/200"
+  },
+  {
+    name : "REDMI",
+    category : "mobile",
+    description : "Xiaomi product",
+    image : "https://picsum.photos/200/200"
+  },
+  {
+    name : "One plus",
+    category : "mobile",
+    description : "Oneplus product",
+    image : "https://picsum.photos/200/200"
+  },
+
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shopping Cart' });
+  res.render('index', { title: 'Shopping Cart', products });
 });
 
 module.exports = router;
