f92c799ea671c97e4a3ed10bfafef43c29c75e0d
Aravind <aravindprakash000@gmail.com>
2024-04-18 11:58:33 +0530

    changes in product-helpers

--- product-helpers/productHelpers.js
+++ product-helpers/productHelpers.js
@@ -4,24 +4,13 @@ const {client, connect} = require("../config/connection");
 const db = client.db('shopping-cart-node');
 
 
-// function addProduct(product, callback){
-//    db.collection('product').insertOne(product).then((data) => {
-//         console.log(`db data is : ${data}`);
-//         callback(true);
-//    })
-// }
-
-function addProduct(product, image){
-    return new Promise((resolve, reject) => {
-        db.collection('product').insertOne(product)
-        .then((result) => {
-            const insertId = result.insertedId.toString();
-
-            const imageFileName = `${insertId}${path.extname(image.originalname)}`;
-            const imagePath = path.join(__dirname, 'public\images', imageFileName)
-        })
-    })
+function addProduct(product, callback){
+   db.collection('product').insertOne(product).then((data) => {
+        console.log(`db data is : ${data}`);
+        callback(true);
+   })
 }
+
 
 module.exports = {
     addProduct,
