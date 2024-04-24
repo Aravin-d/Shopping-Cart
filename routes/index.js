5d4b1b7201a3c0a61cf6e784d01cab03e12ecb26
Aravind <aravindprakash000@gmail.com>
2024-04-24 19:49:25 +0530

    Added login and signup pages

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var productHelpers = require('../product-helpers/productHelpers');
 
-const products = [
-  {
-    name : "Iphone",
-    category : "mobile",
-    description : "Apple product",
-    image : "https://picsum.photos/200/200"
-  },
-  {
-    name : "Samsung",
-    category : "mobile",
-    description : "Samsung product",
-    image : "https://picsum.photos/200/200"
-  },
-  {
-    name : "REDMI",
-    category : "mobile",
-    description : "Xiaomi product",
-    image : "https://picsum.photos/200/200"
-  },
-  {
-    name : "One plus",
-    category : "mobile",
-    description : "Oneplus product",
-    image : "https://picsum.photos/200/200"
-  },
-
-]
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Shopping Cart', products});
+  productHelpers.getProducts().then((products) => {
+    res.render('index', {products})
+  })
 });
+
+router.get('/login', (req, res ,next) => {
+  res.render('login');
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup');
+})
 
 module.exports = router;
